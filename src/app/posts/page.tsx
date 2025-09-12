b44cb89fbfa3d7265362b22172d17ef869240017
dpwl0974 <dpwl0974@gmail.com>
2025-09-12 14:14:38 +0900

    feat: post의 타입 정보를 타입스크립트에게 제공하여 컴파일 체크가 가능하도록 하여 개발자의 실수를 줄일 수 있음

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [posts, setPosts] = useState([]);
@@ -16,7 +17,7 @@ export default function Home() {
     <div className="flex flex-col gap-9">
       <h1>글 목록</h1>
       <ul>
-        {posts.map((post: any) => (
+      {posts.map((post: { id: number; title: string }) => (
           <li key={post.id}>
             {post.id} : {post.title}
           </li>
