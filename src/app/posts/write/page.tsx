bbcd2ccf9b42cc56acf41262973d6597488f954b
dpwl0974 <dpwl0974@gmail.com>
2025-09-15 10:32:24 +0900

    refactor: 디자인 변경 & 글 작성 시 상세페이지로 넘어가기

--- src/app/posts/write/page.tsx
+++ src/app/posts/write/page.tsx
@@ -1,7 +1,10 @@
 "use client"; // 클라이언트 사이드 렌더링 - 만들 때마다 생성
 import { fetchApi } from "@/lib/client";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+    const router = useRouter();
+
     const handleSubmit = (e: any) => {
         e.preventDefault();
 
@@ -28,6 +31,8 @@ export default function Home() {
             }),
         }).then((data) => {
             alert(data.msg);
+
+            router.push(`/posts/${data.data.postDto.id}`);
         });
     };
 
@@ -35,9 +40,20 @@ export default function Home() {
         <>
             <h1 className="text-center">새 글 작성</h1>
             <form className="flex flex-col gap-2 p-2" onSubmit={handleSubmit}>
-                <input type="text" name="title" placeholder="제목" />
-                <textarea name="content" placeholder="내용" />
-                <button type="submit">저장</button>
+                <input
+                    className="border border-gray-300 rounded p-2"
+                    type="text"
+                    name="title"
+                    placeholder="제목"
+                />
+                <textarea
+                    className="border border-gray-300 rounded p-2"
+                    name="content"
+                    placeholder="내용"
+                />
+                <button className="bg-blue-500 text-white p-2 rounded" type="submit">
+                    저장
+                </button>
             </form>
         </>
     );
